9a9f4b3227e9bcf1394451df06111034acf7dc25
Maxime Fleury <Maxime.Fleury-ISC.France@ibm.com>
2018-12-13 10:10:48 +0100

    Ajout de isEtatOuvert() dans Taquin Ajout de listeEtat<Taquin> dans Main N'hesite pas à y retoucher si c'est pas top, c'était surtout des test. Et je crois avoir inversé les états fermés et ouverts ^^

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		ArrayList<Taquin> listeEtatOuvert = new ArrayList<>();
+		ArrayList<Taquin> listeEtatFerme = new ArrayList<>();
+
+		//Taquin à resoudre, etat initiale
 		Taquin t = new Taquin();
-		t.init();
+		listeEtatFerme.add(t);
+		t.init(listeEtatOuvert, listeEtatFerme);
+
+		System.out.println(t.isEtatOuvert(listeEtatOuvert,listeEtatFerme));
 
 
 	}
