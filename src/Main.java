ecc4999f0afa57bd5ad3d03ec5eeb1565df09107
Maxime Fleury <Maxime.Fleury-ISC.France@ibm.com>
2019-01-10 10:25:27 +0100

    Heuristique ok sur 3x3 + Message d'attente

--- src/Main.java
+++ src/Main.java
@@ -3,36 +3,50 @@ import java.util.PriorityQueue;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
         PriorityQueue<Taquin> listeEtatOuvert = new PriorityQueue<Taquin>();
         PriorityQueue<Taquin> listeEtatFerme = new PriorityQueue<>();
-		//Taquin à resoudre, etat initiale
-		Taquin t = new Taquin();
-		t.init(listeEtatOuvert, listeEtatFerme);
-		listeEtatOuvert.add(t);
+        //Taquin à resoudre, etat initiale
+        Taquin t = new Taquin();
+        t.init(listeEtatOuvert, listeEtatFerme);
+        listeEtatOuvert.add(t);
 
+        long debut = System.currentTimeMillis();
+        long tmp = System.currentTimeMillis();
 
-		//System.out.println("1");
-		while(!listeEtatOuvert.isEmpty()) {
-			//System.out.println(t.isEtatFerme(listeEtatFerme));
-			Taquin x = listeEtatOuvert.poll();
-			//x.affiche();
-			listeEtatFerme.add(x);
-			x.createSucc(listeEtatOuvert, listeEtatFerme);
-			for(Taquin y : x.getSuccesseurs()){
-			    y.heuristique(listeEtatOuvert, listeEtatFerme);
+        System.out.println("Veuillez Patienter ...");
+        try {
+            while (!listeEtatOuvert.isEmpty()) {
+                tmp = Main.printWaiting(tmp, System.currentTimeMillis());
+                Taquin x = listeEtatOuvert.poll();
+                //x.affiche();
+                listeEtatFerme.add(x);
+                x.createSucc(listeEtatOuvert, listeEtatFerme);
+                for (Taquin y : x.getSuccesseurs()) {
+                    y.heuristique(listeEtatOuvert, listeEtatFerme);
+                }
+                x.getSuccesseurs().clear();
             }
-			x.getSuccesseurs().clear();
-		}
-		System.out.println("Fin");
 
-	}
+        } catch (
+                OutOfMemoryError e) {
+            System.out.println("\n\nErreur, il n'y a plus de memoire disponible :");
+            e.printStackTrace();
+        }
 
+        System.out.println("\n\nExit in "+Long.toString((System.currentTimeMillis()-debut)/1000)+"s.");
 
+    }
 
+    static long printWaiting(long debut, long tmp) {
+        if (tmp - debut > 5000) {
+            System.out.println("Veuillez Patienter ...");
+            return tmp;
+        }
 
-
+        return debut;
+    }
 
 }
